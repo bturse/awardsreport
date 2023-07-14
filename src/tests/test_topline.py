842f924065ba971a4e90de77bd034774e449bb43
Ben Turse <bturse@Bens-Air.hsd1.ca.comcast.net>
2023-07-14 14:26:51 -0700

    factory helper type hints

--- src/tests/test_topline.py
+++ src/tests/test_topline.py
@@ -1,27 +1,32 @@
 import pytest
 from awardsreport.logic import topline
-from awardsreport.database import Session
+from sqlalchemy.orm import Session
 
 from tests.factories import AssistanceTransactionsFactory
 
 
-def test_get_award_type_month_total(db_session):
-    db_session.add(
-        AssistanceTransactionsFactory(
-            federal_action_obligation=100,
-            action_date="2023-05-01",
-            assistance_transaction_unique_key=1,
-        )
-    )
-    db_session.add(
-        AssistanceTransactionsFactory(
-            federal_action_obligation=100,
-            action_date="2023-05-01",
-            assistance_transaction_unique_key=2,
-        )
+def test_get_award_type_month_total(db_session: Session):
+    db_session.add_all(
+        [
+            AssistanceTransactionsFactory(
+                federal_action_obligation=1, action_date="2023-05-01"
+            ),
+            AssistanceTransactionsFactory(
+                federal_action_obligation=1, action_date="2023-05-31"
+            ),
+            AssistanceTransactionsFactory(
+                federal_action_obligation=-1, action_date="2023-05-01"
+            ),
+            AssistanceTransactionsFactory(
+                federal_action_obligation=1, action_date="2023-06-01"
+            ),
+            AssistanceTransactionsFactory(
+                federal_action_obligation=1, action_date="2022-05-01"
+            ),
+        ]
     )
     db_session.commit()
 
     result = topline.get_award_type_month_total(db_session, "assistance", 2023, 5)
-    expected_result = 200
+    expected_result = 1
     assert result == expected_result
