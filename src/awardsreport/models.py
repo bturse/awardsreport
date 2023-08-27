4d435c986eef827aa1180e8a5a65bfcf8ccec430
Ben Turse <bturse@Bens-Air.hsd1.ca.comcast.net>
2023-08-27 11:16:43 -0700

    refactor raw derived, implement alembic

--- src/awardsreport/models.py
+++ src/awardsreport/models.py
@@ -1,68 +1,55 @@
 from sqlalchemy import String, Float, Date
-from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import mapped_column, Mapped
 
 from awardsreport.database import Base
 
 
-class AssistanceTransactions(Base):
-    __tablename__ = "assistance_transactions"
-
-    # These columns must be declared alphabetically and match
-    # seed_helpers.ASSISTANCE_COLS in name and order.
-    # This is because the download endpoint returns columns in alphabetical
-    # order when columns are specified.
-    # This also prevents us from using a abstract class for shared columns
-    # betwenn AssistanceTransactions and ContractTransactions.
-    action_date = mapped_column(Date)
-    assistance_award_unique_key = mapped_column(String, nullable=False)
-    assistance_transaction_unique_key = mapped_column(
-        String, primary_key=True, autoincrement=False
-    )
-    assistance_type_code = mapped_column(String(2))
-    awarding_agency_code = mapped_column(String(4))
-    awarding_agency_name = mapped_column(String)
-    awarding_office_code = mapped_column(String(6))
-    awarding_office_name = mapped_column(String)
-    awarding_sub_agency_code = mapped_column(String(4))
-    awarding_sub_agency_name = mapped_column(String)
-    cfda_number = mapped_column(String(6))
-    cfda_title = mapped_column(String)
+class TransactionsMixin:
+    action_date: Mapped[Date] = mapped_column(Date)
+    awarding_agency_code: Mapped[str] = mapped_column(String)
+    awarding_agency_name: Mapped[str] = mapped_column(String)
     federal_action_obligation = mapped_column(Float)
-    original_loan_subsidy_cost = mapped_column(Float)
-    primary_place_of_performance_congressional_district = mapped_column(String)
-    primary_place_of_performance_country_code = mapped_column(String)
-    primary_place_of_performance_country_name = mapped_column(String)
-    primary_place_of_performance_county_name = mapped_column(String)
     primary_place_of_performance_state_name = mapped_column(String)
-    prime_award_transaction_place_of_performance_county_fips_code = mapped_column(
-        String(5)
-    )
-    prime_award_transaction_place_of_performance_state_fips_code = mapped_column(
-        String(2)
-    )
+    recipient_name = mapped_column(String)
+    recipient_uei = mapped_column(String)
+    usaspending_permalink = mapped_column(String)
 
 
-class ProcurementTransactions(Base):
-    __tablename__ = "procurement_transactions"
-
-    action_date = mapped_column(Date)
-    awarding_agency_code = mapped_column(String(4))
-    awarding_agency_name = mapped_column(String)
-    awarding_office_code = mapped_column(String(6))
-    awarding_office_name = mapped_column(String)
-    awarding_sub_agency_code = mapped_column(String(4))
-    awarding_sub_agency_name = mapped_column(String)
+class ProcurementTransactionsMixin:
     contract_award_unique_key = mapped_column(String, nullable=False)
     contract_transaction_unique_key = mapped_column(String, primary_key=True)
-    federal_action_obligation = mapped_column(Float)
-    primary_place_of_performance_congressional_district = mapped_column(String)
-    primary_place_of_performance_country_code = mapped_column(String)
-    primary_place_of_performance_country_name = mapped_column(String)
-    primary_place_of_performance_county_name = mapped_column(String)
-    primary_place_of_performance_state_name = mapped_column(String)
-    prime_award_transaction_place_of_performance_county_fips_code = mapped_column(
-        String(5)
-    )
-    prime_award_transaction_place_of_performance_state_fips_code = mapped_column(
-        String(2)
-    )
+    naics_code = mapped_column(String)
+    naics_description = mapped_column(String)
+    product_or_service_code = mapped_column(String)
+    product_or_service_code_description = mapped_column(String)
+
+
+class AssistanceTransactionsMixin:
+    assistance_award_unique_key = mapped_column(String, nullable=False)
+    assistance_transaction_unique_key = mapped_column(String, primary_key=True)
+    assistance_type_code = mapped_column(String(2))
+    cfda_number = mapped_column(String(6))
+    cfda_title = mapped_column(String)
+    original_loan_subsidy_cost = mapped_column(Float)
+
+
+class TransactionDerivationsMixin:
+    generated_pragmatic_obligations = mapped_column(String)
+
+
+class AssistanceTransactions(
+    Base,
+    TransactionsMixin,
+    AssistanceTransactionsMixin,
+    TransactionDerivationsMixin,  # must be inhereted last
+):
+    __tablename__ = "assistance_transactions"
+
+
+class ProcurementTransactions(
+    Base,
+    TransactionsMixin,
+    ProcurementTransactionsMixin,
+    TransactionDerivationsMixin,  # must be inhereted last
+):
+    __tablename__ = "procurement_transactions"
--- src/awardsreport/seed.py
+++ src/awardsreport/seed.py
@@ -8,7 +8,7 @@ import logging
 
 from awardsreport.database import engine, Base
 from awardsreport.models import ProcurementTransactions, AssistanceTransactions
-from awardsreport.helpers.seed_helpers import (
+from awardsreport.seed_helpers import (
     get_awards_payloads,
     generate_copy_from_sql,
     YEAR,
@@ -67,29 +67,27 @@ def awards_usas_to_sql(year, month, no_months, period_months=12):
         if status == "finished":
             logging.info(f"status: {status}")
             logging.info(f"requesting: {r['file_url']}")
-            file = requests.get(r["file_url"], stream=True)
+            try:
+                file = requests.get(r["file_url"], stream=True)
+                with tempfile.TemporaryDirectory() as raw_data:
+                    logging.info(f"temp: {raw_data}")
+                    with ZipFile(BytesIO(file.content), "r") as zip_ref:
+                        logging.info("extracting zip data")
+                        zip_ref.extractall(raw_data)
+                        files = glob("*.csv", root_dir=raw_data)
+                        logging.info(f"files: {files}")
+                        for file in files:
+                            logging.info(f"file: {file}")
+                            copy_cmd = generate_copy_from_sql(file)
 
-        with tempfile.TemporaryDirectory() as raw_data:
-            logging.info(f"temp: {raw_data}")
-            with ZipFile(BytesIO(file.content), "r") as zip_ref:
-                logging.info("extracting zip data")
-                zip_ref.extractall(raw_data)
-                files = glob("*.csv", root_dir=raw_data)
-                logging.info(f"files: {files}")
-                for file in files:
-                    logging.info(f"file: {file}")
-                    copy_cmd = generate_copy_from_sql(file)
-
-                    logging.info(f"copy_cmd: {copy_cmd}")
-                    with open(f"{raw_data}/{file}", "r") as f:
-                        cursor.copy_expert(copy_cmd, f)
-                        conn.commit()
-                        logging.info("commit completed")
+                            logging.info(f"copy_cmd: {copy_cmd}")
+                            with open(f"{raw_data}/{file}", "r") as f:
+                                cursor.copy_expert(copy_cmd, f)
+                                conn.commit()
+                                logging.info("commit completed")
+            except Exception:
+                raise Exception(f"Unable to import {r['file_url']}")
 
 
 if __name__ == "__main__":
-    Base.metadata.drop_all(engine)
-    logging.info("dropped metadata")
-    Base.metadata.create_all(engine)
-    logging.info("created metadata")
     awards_usas_to_sql(YEAR, MONTH, 1, 1)
