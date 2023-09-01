9d2e5efed66155e92f13733a646c778ee4628d1a
Ben Turse <bturse@Bens-Air.hsd1.ca.comcast.net>
2023-08-31 20:47:48 -0700

    change logic

--- src/awardsreport/logic/summary_tables.py
+++ src/awardsreport/logic/summary_tables.py
@@ -1,32 +1,25 @@
-from sqlalchemy import Column, select, func, desc, Float
+from sqlalchemy import select, func, desc, Table
 from typing import Optional
 from sqlalchemy.orm import Session
-from awardsreport.models import AssistanceTransactions
-from pydantic import BaseModel
 from sqlalchemy.orm import Mapped
 
 sess = Session()
 
 
 def groupby_sum(
-    #    sess: Session,
+    sess: Session,
     group_by_cols: Mapped[Optional[str]] | list[Mapped[Optional[str]]],
     sum_col: Mapped[Optional[float]],
+    limit: int = 10,
 ):
     if not isinstance(group_by_cols, list):
         group_by_cols = [group_by_cols]
+    table: Table = group_by_cols[0].class_  # type: ignore (pylance does not recognice .class_)
     stmt = (
         select(*group_by_cols, func.sum(sum_col).label("ct"))
         .group_by(*group_by_cols)
         .order_by(desc("ct"))
+        .limit(limit)
+        .where()
     )
     return stmt
-
-
-if __name__ == "__main__":
-    print(type(AssistanceTransactions.awarding_agency_name))
-    stmt = groupby_sum(
-        group_by_cols=AssistanceTransactions.awarding_agency_name,
-        sum_col=AssistanceTransactions.generated_pragmatic_obligations,
-    )
-    print(stmt)
